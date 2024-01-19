d0fda54f06ad1363e1a1a9e642c52c3cf69e0a35
santi <santizuleta11@gmail.com>
2024-01-19 12:15:03 +0100

    Arregla el navbar

--- linkjurist_vue/src/components/Navbar.vue
+++ linkjurist_vue/src/components/Navbar.vue
@@ -1,23 +1,24 @@
 <script setup>
     import { useAuthStore } from '@/stores/auth';
+    import { RouterLink } from 'vue-router';
+    import { ref } from 'vue'
+
     const store = useAuthStore();
+    
+    const showMobileMenu = ref(false);
+    const showDropdownMenu = ref(false);
+
 </script>
 
 <template>
 <nav class="navbar main-bg-color is-spaced" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-    <RouterLink to="/" class="navbar-item is-size-4-desktop is-size-5-touch mr-5">
+    <RouterLink to="/" class="navbar-item is-size-4-desktop is-size-5-touch mr-5 text-white">
       <img src="../assets/logo.png" width="58" height="112">
-      <!-- <i class="fa-solid fa-scale-balanced"></i> -->
       <span class="has-text-weight-medium is-hidden-touch secondary-text-color">Link Jurist</span>
       <span class="has-text-weight-normal is-hidden-desktop secondary-text-color">Link Jurist</span>
     </RouterLink>
 
-    <!-- <a class="navbar-item is-size-4-desktop is-size-5-touch mr-5">
-      <img src="../assets/logo.png" width="58" height="112">
-      <span class="has-text-weight-medium is-hidden-touch secondary-text-color">Link Jurist</span>
-      <span class="has-text-weight-normal is-hidden-desktop secondary-text-color">Link Jurist</span>
-    </a> -->
 
     <a role="button" class="navbar-burger white-text" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" @click="showMobileMenu = !showMobileMenu">
       <span aria-hidden="true"></span>
@@ -50,7 +51,7 @@
     </div>
 
     <div class="navbar-end">
-      <div v-if="store.authenticated" class="navbar-item has-dropdown is-hoverable">
+      <div v-if="store.authenticated" class="navbar-item has-dropdown is-hoverable" @click="showDropdownMenu = !showDropdownMenu">
         <a class="navbar-link white-text is-hidden-touch">
           Menú
         </a>
@@ -59,7 +60,6 @@
         </a>
 
         <div class="navbar-dropdown is-right">
-
           <RouterLink to="/account" class="navbar-item">Mi cuenta</RouterLink>
           <a v-on:click="store.changeAuthenticatedVal()" class="navbar-item">Cerrar sesión</a>
 
@@ -69,19 +69,6 @@
         <RouterLink id="signup_btn" class="button is-rounded secondary-bg-color has-text-weight-semibold white-text is-responsive navbar-button" to="signup" style="width: 150px;">Registro</RouterLink>
         <RouterLink id="login_btn" class="button is-rounded white-bg-color has-text-weight-semibold is-responsive navbar-button main-text-color" to="login" style="width: 150px;">Iniciar sesión</RouterLink>
       </div>
-
-
-
-      <!-- <div class="navbar-item is-hidden-touch">
-        <div v-if="!store.authenticated" class="buttons buttons are-normal">
-          <RouterLink id="signup_btn" class="button is-rounded secondary-bg-color has-text-weight-semibold white-text is-responsive navbar-button" to="signup" style="width: 150px;">Registro</RouterLink>
-          <RouterLink id="login_btn" class="button is-rounded white-bg-color has-text-weight-semibold is-responsive navbar-button main-text-color" to="login" style="width: 150px;">Iniciar sesión</RouterLink>
-        </div>
-        <div v-else>
-          <a v-on:click="store.changeAuthenticatedVal()" class="button is-rounded secondary-bg-color has-text-weight-semibold white-text is-responsive navbar-button">Cerrar sesión</a>
-        </div>
-      </div> -->
-
     </div>
   </div>
 </nav>
@@ -89,16 +76,5 @@
 </template>
 
 <script>
-import { RouterLink } from 'vue-router';
-
-export default {
-    data() {
-        return {
-            showMobileMenu: false,
-            handleDropdownMenu: false,
-        };
-    },
-    components: { RouterLink }
-}
 
 </script>
